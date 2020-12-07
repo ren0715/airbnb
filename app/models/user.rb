c18a4445141deca5d6564c3fef8c808d890f026b
ren0715 <71118115+ren0715@users.noreply.github.com>
2020-12-07 16:10:35 +0900

    [Function] Gravatar (#8)
    * [Function] Gravatar
    
    * Fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def get_gravatar
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  end
 end
