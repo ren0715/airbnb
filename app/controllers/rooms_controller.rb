2d83a421703c7998d7ba20217aa160800a1f897a
ren0715 <r.s.izxo715@gmail.com>
2020-12-10 15:55:11 +0900

    Fixed

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -20,7 +20,8 @@ class RoomsController < ApplicationController
   end
 
   def update
-    if @room.update(room_params)
+    @final_params = is_ready?(@room) ? room_params.merge(is_active: true) : room_params
+    if @room.update(@final_params)
       redirect_to request.referrer
     else
       render 'room'
@@ -48,7 +49,7 @@ class RoomsController < ApplicationController
 
   private
   def room_params
-    params.require(:room).permit(:home_type, :room_type, :guest_count, :bedroom_count, :bathroom_count, :price, :room_name, :summary, :address, :has_tv, :has_kitchen, :has_internet, :has_heating, :has_air_conditioning, :is_active)
+    params.require(:room).permit(:home_type, :room_type, :guest_count, :bedroom_count, :bathroom_count, :price, :room_name, :summary, :address, :has_tv, :has_kitchen, :has_internet, :has_heating, :has_air_conditioning)
   end
 
   def get_room
--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,9 +1,7 @@
 module RoomsHelper
 
   def is_ready?(room)
-    if !room.price.blank? && !room.photos.blank? && !room.room_name.blank? && !room.address.blank?
-      return true
-    end
+    !room.price.blank? && !room.photos.blank? && !room.room_name.blank? && !room.address.blank?
   end
   
 end
