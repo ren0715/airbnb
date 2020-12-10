17ea5e93d2ddfc17ee5e202816da5c275076f04a
ren0715 <r.s.izxo715@gmail.com>
2020-12-10 15:37:26 +0900

    [Function] Publish button

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,9 @@
 module RoomsHelper
+
+  def is_ready?(room)
+    if !room.price.blank? && !room.photos.blank? && !room.room_name.blank? && !room.address.blank?
+      return true
+    end
+  end
+  
 end
