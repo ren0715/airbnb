44db3a86e9eb9736eefb47bc88db861c8448a886
ren0715 <71118115+ren0715@users.noreply.github.com>
2020-12-11 15:04:02 +0900

    [Layout] Profile (#17)

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  resources :users
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
