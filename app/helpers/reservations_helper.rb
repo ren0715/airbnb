ebdd816a7d42186b675aa6d478f66a9a4e434200
ren0715 <r.s.izxo715@gmail.com>
2020-12-15 15:30:45 +0900

    Fixed

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,15 +1,3 @@
 module ReservationsHelper
-  def create
-    @reservation = current_user.reservations.new(reservation_params)
-
-    if @reservation.save
-      redirect_to root_url
-    else
-      render 'new'
-    end
-  end
-  private
-  def reservation_params
-    params.require(:reservation).permit(:start_date, :end_date, :price, :total_price, :room_id)
-  end
+  
 end
