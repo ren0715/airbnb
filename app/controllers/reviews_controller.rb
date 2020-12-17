b1a3f8149bb984079f11880437ae7325063e01fd
ren0715 <r.s.izxo715@gmail.com>
2020-12-17 15:44:19 +0900

    [Function] Review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    
+    if @review.save
+      flash[:notice] = "Posted Successfully!"
+      redirect_to request.referrer
+    else
+      flash[:alert] = "You failed! #{@review.content}:#{@review.host_id}:#{@review.guest_id}"
+      redirect_to request.referrer
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:host_id, :guest_id, :room_id, :content,:rate)
+  end
 end
