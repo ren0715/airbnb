54ebf20d1fb6577e4b502ffb4f6ca9ab7ce76a3a
ren0715 <r.s.izxo715@gmail.com>
2020-12-17 18:27:59 +0900

    Fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,20 +2,21 @@ class ReviewsController < ApplicationController
 
   def create
     @reservation = Reservation.find(params[:reservation_id])
-    @review = Review.new(review_params)
+    @review = @reservation.reviews.new(review_params)
     
-    if @reservation.review
+    if @reservation.reviews.where(host_id: @review.host_id, guest_id: @review.guest_id).any?
       flash[:alert] = "You already reviewed this reservation."
       redirect_to request.referrer
     else
-      @review.save
-      flash[:notice] = "Reviewed Successfully!"
-      redirect_to request.referrer
+      if @review.save
+        flash[:notice] = "Reviewed Successfully!"
+        redirect_to request.referrer
+      end
     end
   end
 
   private
   def review_params
-    params.require(:review).permit(:host_id, :guest_id, :content, :reservation_id)
+    params.require(:review).permit(:host_id, :guest_id, :content, :rate)
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  has_one :review
+  has_many :reviews
 end
