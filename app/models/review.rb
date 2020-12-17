69e1c795dbcce7055a154d21d932a4790670779a
ren0715 <r.s.izxo715@gmail.com>
2020-12-17 18:35:54 +0900

    fixed2

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :reservation
-  # validates :rate, numericality: {
-  #   less_than_or_equal_to: 5,
-  #   greater_than_or_equal_to: 1
-  # }, presence: true
-  # validates :content, presence: true
+  validates :content, presence: true
 end
