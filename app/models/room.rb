1af858b0e52b46a8c23dd0f78f12f56acadf7f06
ren0715 <r.s.izxo715@gmail.com>
2020-12-18 22:05:04 +0900

    Fixed

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
   has_many :reservations, dependent: :destroy
+  has_many :reviews, through: :reservations
+
   validates :home_type, presence: true
   validates :room_type, presence: true
   validates :guest_count, presence: true
@@ -10,4 +12,24 @@ class Room < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+
+  def average_rating
+    ids = reservations.pluck(:user_id)# pluck all the user_id from the reservations
+    
+    # add a condition to return 0 if the reviews from guests is 0
+    if reviews.where(user_id: ids).any?
+      reviews.where(user_id: ids).average(:rate).round(2).to_i
+    else
+      return 1
+    end
+  end
+
+  def guest_reviews
+    ids = reservations.pluck(:user_id)
+    reviews.where(user_id: ids)
+  end
+  def host_reviews
+    ids = reservations.pluck(:user_id)
+    reviews.where.not(user_id: ids)
+  end
 end
